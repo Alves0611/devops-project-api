9ce66bb8f6938b429347e21185aa695710acd985
Alves0611 <g.alves0611@gmail.com>
2024-06-15 19:20:47 -0300

    test: testing for get user

--- tests/test_application.py
+++ tests/test_application.py
@@ -3,6 +3,7 @@ from application import create_app
 
 
 class TestApplication():
+
     @pytest.fixture
     def client(self):
         app = create_app('config.MockConfig')
@@ -11,21 +12,21 @@ class TestApplication():
     @pytest.fixture
     def valid_user(self):
         return {
-            "first_name": "Gabriel",
-            "last_name": "Alves",
-            "cpf": "430.703.818-55",
-            "email": "gabriel0611@gmail.com",
-            "birth_date": "2002-11-06"
+            "first_name": "Mateus",
+            "last_name": "Muller",
+            "cpf": "641.396.500-28",
+            "email": "contato@mateusmuller.me",
+            "birth_date": "1996-09-10"
         }
-    
+
     @pytest.fixture
     def invalid_user(self):
         return {
-            "first_name": "Gabriel",
-            "last_name": "Alves",
-            "cpf": "430.703.818-55",
-            "email": "gabriel0611@gmail.com",
-            "birth_date": "2002-11-06"
+            "first_name": "Mateus",
+            "last_name": "Muller",
+            "cpf": "641.396.500-27",
+            "email": "contato@mateusmuller.me",
+            "birth_date": "1996-09-10"
         }
 
     def test_get_users(self, client):
@@ -37,6 +38,21 @@ class TestApplication():
         assert response.status_code == 200
         assert b"successfully" in response.data
 
-        response = client.post('/user', json=valid_user)
+        response = client.post('/user', json=invalid_user)
+        assert response.status_code == 400
+        assert b"invalid" in response.data
+
+    def test_get_user(self, client, valid_user, invalid_user):
+        response = client.get('/user/%s' % valid_user["cpf"])
         assert response.status_code == 200
-        assert b"successfully" in response.data
+        assert response.json[0]["first_name"] == "Mateus"
+        assert response.json[0]["last_name"] == "Muller"
+        assert response.json[0]["cpf"] == "641.396.500-28"
+        assert response.json[0]["email"] == "contato@mateusmuller.me"
+
+        birth_date = response.json[0]["birth_date"]["$date"]
+        assert birth_date == "1996-09-10T00:00:00Z"
+
+        response = client.get('/user/%s' % invalid_user["cpf"])
+        assert response.status_code == 400
+        assert b"User does not exist in database!" in response.data
